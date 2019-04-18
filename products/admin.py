904d94a4b880b383424cb10c7349cc011ed33c5f
zzzolegus <zzzorgjb@gmail.com>
2019-04-18 15:49:55 +0300

    Ajax bin complete

--- products/admin.py
+++ products/admin.py
@@ -1,40 +1,45 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-#==============================================================
+
 
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 0
-    
-#==============================================================
-    
-class ProductAdmin(admin.ModelAdmin):
 
-    list_display = [fields.name for fields in Product._meta.fields]
-    inlines = [ProductImageInline]
- 
-    class Meta:
-        model = Product        
-admin.site.register(Product, ProductAdmin)  
 
-#==============================================================
-
-    
 class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductCategory._meta.fields]
 
-    list_display = [fields.name for fields in ProductCategory._meta.fields]
- 
     class Meta:
-        model = ProductCategory        
-admin.site.register(ProductCategory, ProductCategoryAdmin) 
+        model = ProductCategory
 
-#==============================================================
+admin.site.register(ProductCategory, ProductCategoryAdmin)
 
-class ProductImageAdmin(admin.ModelAdmin):
 
-    list_display = [fields.name for fields in ProductImage._meta.fields]
- 
+class ProductAdmin (admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+    inlines = [ProductImageInline]
+
     class Meta:
-        model = ProductImage       
-admin.site.register(ProductImage, ProductImageAdmin)  
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductImageAdmin (admin.ModelAdmin):
+    list_display = [field.name for field in ProductImage._meta.fields]
+
+    class Meta:
+        model = ProductImage
+
+admin.site.register(ProductImage, ProductImageAdmin)
+
+
+
+
+
+
+
+
+
+
--- products/views.py
+++ products/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .models import Product
-
-# Create your views here.
-
+from products.models import *
 
 
 def product(request, product_id):
-	product = Product.objects.get(id=product_id)
-	return render(request, 'products/product.html', locals())
+    product = Product.objects.get(id=product_id)
 
+
+    session_key = request.session.session_key
+    if not session_key:
+        request.session.cycle_key()
+
+    print(request.session.session_key)
+
+
+    return render(request, 'products/product.html', locals())
