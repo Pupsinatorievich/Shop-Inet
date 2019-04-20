cd0734ae7a82f00fb75b03bd3a17e34290d2018b
zzzolegus <zzzorgjb@gmail.com>
2019-04-20 09:06:46 +0300

    satge 1.9.2

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,8 @@
 from django.http import JsonResponse
-from .models import ProductInBasket
+from .models import *
+from django.shortcuts import render
+from .forms import *
+from django.contrib.auth.models import User
 
 
 def basket_adding(request):
@@ -37,3 +40,61 @@ def basket_adding(request):
     print(return_dict)
     
     return JsonResponse(return_dict)
+
+def checkout(request):
+    session_key = request.session.session_key
+    products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
+    form = CheckoutContactForm(request.POST or None)
+    if request.POST:
+        print(request.POST)
+        if form.is_valid():
+            print("form valid")
+            data = request.POST
+            name = data.get("name", "64565441")
+            phone = data["phone"]
+            user, created = User.objects.get_or_create(username=phone, defaults={"first_name":name})
+
+            order = Order.objects.create(user=user,
+                                         customer_name=name, customer_phone=phone,status_id=1)
+
+            for name, value in data.items():
+                if name.startswith("product_in_basket_"):
+                    product_in_basket_id = name.split("product_in_basket_")[1]
+                    product_in_basket = ProductInBasket.objects.get(id=product_in_basket_id)
+                    product_in_basket.nmb = value
+                    product_in_basket.save(force_update=True)
+
+                    ProductInOrder.objects.create(product=product_in_basket.product,
+                                                  nmb=product_in_basket.nmb,
+                                                  price_per_item=product_in_basket.price_per_item,
+                                                  total_price=product_in_basket.total_price,
+                                                  order=order)
+
+        else:
+            print("not valid form")
+
+    return render(request, 'orders/checkout.html', locals())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
